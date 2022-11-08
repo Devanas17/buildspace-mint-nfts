1eed892f2b3f9c2067e7203a8573a05e4729b0e4
DevAnas17 <anasdeveloper17@gmail.com>
2022-11-08 21:37:41 +0530

    add randomly generated nft

--- smart_contracts/contracts/MyEpicNFT.sol
+++ smart_contracts/contracts/MyEpicNFT.sol
@@ -5,46 +5,120 @@ pragma solidity ^0.8.7;
 // import some OpenZeppelin Contracts.
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "hardhat/console.sol";
 
-contract MyEpicNFT is ERC721 {
+contract MyEpicNFT is ERC721URIStorage {
     // Magic given to us by OpenZeppelin to help us keep track of tokenIds.
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    // We need to pass the name of our NFTs token and its symbol.
+    // This is our SVG code. All we need to change is the word that's displayed. Everything else stays the same.
+    // So, we make a baseSvg variable here that all our NFTs can use.
+    string baseSvg =
+        "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+    // I create three arrays, each with their own theme of random words.
+    // Pick some random funny words, names of anime characters, foods you like, whatever!
+    string[] firstWords = [
+        "CRICKET-",
+        "BASKETBALL-",
+        "FOOTBALL-",
+        "HOCKEY-",
+        "TENNIS-",
+        "KABADDI-"
+    ];
+    string[] secondWords = [
+        "VIRAT_KOHLI",
+        "RONALDO",
+        "MESSI",
+        "DEVILLIERS",
+        "MS_DHONI",
+        "DAYLE_STEYN"
+    ];
+    string[] thirdWords = [
+        "-JAVASCRIPT",
+        "-SOLIDITY",
+        "-PYTHON",
+        "-JAVA",
+        "-RUST",
+        "-GO"
+    ];
 
     constructor() ERC721("SquareNFT", "SQUARE") {
-        console.log("This is my NFT contract. Whoa!");
+        console.log("This is my NFT contract. Woah!");
     }
 
-    // A function our user will hit to get their NFT.
-    function makeAnEpicNFT() public {
-        // Get the current tokenId, this starts at 0.
-        uint256 newItemId = _tokenIds.current();
-
-        // Actually mint the NFT to the sender using msg.sender.
-        _safeMint(msg.sender, newItemId);
-
-        // Increment the counter for when the next NFT is minted.
-        _tokenIds.increment();
-    }
-
-    // Set the NFT's metadata
-    function tokenURI(uint256 _tokenId)
+    // I create a function to randomly pick a word from each array.
+    function pickRandomFirstWord(uint256 tokenId)
         public
         view
-        override
         returns (string memory)
     {
-        require(_exists(_tokenId));
+        // I seed the random generator. More on this in the lesson.
+        uint256 rand = random(
+            string(abi.encodePacked("CRICKET", Strings.toString(tokenId)))
+        );
+        // Squash the # between 0 and the length of the array to avoid going out of bounds.
+        rand = rand % firstWords.length;
+        return firstWords[rand];
+    }
+
+    function pickRandomSecondWord(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("BASKETBALL", Strings.toString(tokenId)))
+        );
+        rand = rand % secondWords.length;
+        return secondWords[rand];
+    }
+
+    function pickRandomThirdWord(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("FOOTBALL", Strings.toString(tokenId)))
+        );
+        rand = rand % thirdWords.length;
+        return thirdWords[rand];
+    }
+
+    function random(string memory input) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(input)));
+    }
+
+    function makeAnEpicNFT() public {
+        uint256 newItemId = _tokenIds.current();
+
+        // We go and randomly grab one word from each of the three arrays.
+        string memory first = pickRandomFirstWord(newItemId);
+        string memory second = pickRandomSecondWord(newItemId);
+        string memory third = pickRandomThirdWord(newItemId);
+
+        // I concatenate it all together, and then close the <text> and <svg> tags.
+        string memory finalSvg = string(
+            abi.encodePacked(baseSvg, first, second, third, "</text></svg>")
+        );
+        console.log("\n--------------------");
+        console.log(finalSvg);
+        console.log("--------------------\n");
+
+        _safeMint(msg.sender, newItemId);
+
+        // We'll be setting the tokenURI later!
+        _setTokenURI(newItemId, "blah");
+
+        _tokenIds.increment();
         console.log(
             "An NFT w/ ID %s has been minted to %s",
-            _tokenId,
+            newItemId,
             msg.sender
         );
-        return
-            "https://jsonkeeper.com/b/RUUS?utm_source=buildspace.so&utm_medium=buildspace_project";
     }
 }
--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -1,7 +1,31 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const Matic_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+const MUMBAI_PRIVATE_KEY = process.env.MUMBAI_PRIVATE_KEY;
+const GOERLI_API_KEY = process.env.GOERLI_API_KEY;
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
+
 module.exports = {
   defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: GOERLI_API_KEY,
+      accounts: [GOERLI_PRIVATE_KEY],
+    },
+    matic: {
+      url: Matic_API_KEY_URL,
+      accounts: MUMBAI_PRIVATE_KEY !== undefined ? [MUMBAI_PRIVATE_KEY] : [],
+      saveDeployments: true,
+      chainId: 80001,
+    },
+  },
   solidity: "0.8.17",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
 };
